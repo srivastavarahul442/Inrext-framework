a7d2fa4cca2f918178a47df32927b1207b0b68fd
srivastavarahul442 <srivastavarahul442@gmail.com>
2025-12-15 15:56:07 +0530

    Refactor: Fix dependencies and make Mailer configurable

--- index.js
+++ index.js
@@ -1,29 +1,7 @@
-require('dotenv').config();
-const migrate = require('./migration');
-const mailer = require('./mail');
-
-// Run migration
-migrate();
-
-// Send demo email
-const sendDemoEmail = async () => {
-  try {
-    await mailer.sendEmail(
-      'rahulmithu5@gmail.com',
-      'Demo Email from Inrext Framework',
-      '<h1>Hello!</h1><p>This is a demo email from the Inrext Framework.</p>'
-    );
-    console.log('Demo email sent successfully!');
-  } catch (error) {
-    console.error('Error sending email:', error.message);
-  }
-};
-
-// Uncomment the line below to send demo email
-sendDemoEmail();
+const migrate = require("./migration");
+const mailer = require("./mail");
 
 module.exports = {
   migrate,
-  mailer,
-  sendDemoEmail,
+  mailer
 };
